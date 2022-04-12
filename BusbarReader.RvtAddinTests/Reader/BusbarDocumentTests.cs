c625ca1d885aa499e978fc74a82d05e5dc29ea30
dududu <1831197727@qq.com>
2022-04-12 17:46:32 +0800

    add test project

---
+++ BusbarReader.RvtAddinTests/Reader/BusbarDocumentTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using NUnit.Framework;
+
+namespace BusbarReader.RvtAddin.Reader.Tests
+{
+    [TestFixture()]
+    public class BusbarDocumentTests
+    {
+        public static string Dir = Path.Combine(typeof(BusbarDocumentTests).Assembly.Location, "TestModel");
+
+        [Test]
+        public void VecTest(UIApplication uiApplication, Application application)
+        {
+            var model = Path.Combine(Dir, "竖直折弯.rvt");
+            var busbar = GetBusbar(uiApplication, model,out var doc);
+            
+            Assert.IsNotNull(busbar);
+            Assert.IsTrue(busbar.Count == 1);
+
+            
+        }
+
+        [Test]
+        public void HorTest(UIApplication uiApplication, Application application)
+        {
+            var model = Path.Combine(Dir, "水平折弯.rvt");
+            var busbar = GetBusbar(uiApplication, model, out var doc);
+
+            Assert.IsNotNull(busbar);
+            Assert.IsTrue(busbar.Count == 1);
+        }
+
+        [Test]
+        public void MutiTest(UIApplication uiApplication, Application application)
+        {
+            var model = Path.Combine(Dir, "多铜牌.rvt");
+            var busbar = GetBusbar(uiApplication, model, out var doc);
+
+            Assert.IsNotNull(busbar);
+            Assert.IsTrue(busbar.Count == 1);
+        }
+
+        private static List<Busbar> GetBusbar(UIApplication uiApplication, string model,out Document doc)
+        {
+            doc = uiApplication.Application.OpenDocumentFile(model);
+
+            var busbarDoc = new BusbarDocument(doc);
+            var busbar = busbarDoc.GetBusbar();
+            return busbar;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# 调试
+
+1.选择你的Revit版本
+
+![](Resources/Debug.png)
+
+2.启动Revit
+
+3.附加进程到Revit
